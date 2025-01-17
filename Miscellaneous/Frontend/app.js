07ee0826cc472dfa1efb0f731b9bb775c3289411
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-01-17 22:39:08 +0530

    Inheritance added

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -1,23 +1,23 @@
-const stu1 = {
-    name: "adam",
-    age: 25,
-    marks: 98,
-    getMarks: function () {
-        return this.marks;
-    }
-};
+// const stu1 = {
+//     name: "adam",
+//     age: 25,
+//     marks: 98,
+//     getMarks: function () {
+//         return this.marks;
+//     }
+// };
 
-const stu2 = {
-    name: "peter",
-    age: 21,
-    marks: 78,
-};
+// const stu2 = {
+//     name: "peter",
+//     age: 21,
+//     marks: 78,
+// };
 
-const stu3 = {
-    name: "krish",
-    age: 35,
-    marks: 91,
-}; 
+// const stu3 = {
+//     name: "krish",
+//     age: 35,
+//     marks: 91,
+// }; 
 
 let arr = [1,2,3];
 arr.sayHello = () => {
@@ -46,4 +46,46 @@ function Person(name, age){
     this.name = `${name}`;
     this.age = `${age}`;
 }
+//prototype
 let p3 = new Person("GAJENDRA", 21);
+let p4 = new Person("krrishika", 18);
+
+// using an another better alternative by using classes
+
+class Personn{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    talk(){
+        console.log(`Hi, my name is ${this.name}`);
+    }
+}
+let p5 = new Personn("Adam", 25);
+let p6 = new Personn("eve", 22);
+
+class PersonCommon{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    talk(){
+        console.log(`Hi, my name is ${this.name}`);
+    }
+}
+
+class Student extends PersonCommon{
+    constructor(name, age, marks){
+       super(name, age); //Parent class constructot is being called.
+       this.marks=marks;
+    }
+}
+// let stu1 = new Student("Chris", 25);
+// let stu2 = new Student("Peter", 21);
+
+class Teacher extends PersonCommon{
+    constructor(name, age, subject){
+        super(name, age); // parent class constructor is being called.
+        this.subject = subject;
+    }
+}
