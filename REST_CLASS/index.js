d1769049a8cb36acb7bb8bd6bb92c3676d959859
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-01-19 14:42:35 +0530

    added UUID

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+// uuidv4(); 
 
 app.use(express.urlencoded({extended : true}));
 
@@ -14,14 +16,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id: uuidv4(),
         username: "ApnaCollege",
         content: "I love coding",
     },
     {
+        id: uuidv4(),
         username: "gajendraarya",
         content: "I love playing guitar",
     },
     {
+        id: uuidv4(),
         username: "rahulkumar",
         content: "I love cooking",
     },
@@ -37,11 +42,24 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let {username, content} = req.body;
-    posts.push({username, content});
-    res.send("working");
+    let id = uuidv4(); 
+    posts.push({id, username, content});
+    res.redirect("/posts");
 });
+
+app.get("/posts/:id", (req, res) => {
+      let {id} = req.params;
+      console.log(id);
+      let post = posts.find((p) => id===p.id);
+      res.render("show.ejs", {post});
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    console.log(id);
+    res.send("Path req working");
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
- 
